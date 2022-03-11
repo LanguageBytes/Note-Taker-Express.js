ebafa1f56b190af7867f85663a22f237fafbe520
Sarah Lloyd <sarahlloyd407a@gmail.com>
2022-03-11 16:18:38 +0000

    setting up get and post requests

--- server.js
+++ server.js
@@ -2,19 +2,31 @@
 const fs = require("fs");
 const express = require("express");
 const path = require("path");
-const port = 3000;
+const port = 3002;
 
-
+// Setting Up Server
 const app = express()
 app.use(express.json());
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
-
-app.get('/', (req, res) => {
-    res.send('Hello')
-  });
+// GET Requests
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
   
-  app.listen(port, () => {
-    console.log(`listening on port ${port}!`)
+app.get('/api/notes', (req, res) => {
+    res.send('')
   });
+
+app.get('*', (req, res) => {
+    res.send('')
+  });
+
+  
+// POST Requests
+
+
+  app.listen(port, () =>
+  console.log(`Server listening at: http://localhost:${port}!`)
+);
