ac59c279b62732bf756d8db17c02b7b6231e5bf7
Sarah Lloyd <sarahlloyd407a@gmail.com>
2022-03-11 15:29:04 +0000

    Setting up server app, up and running

--- server.js
+++ server.js
@@ -2,3 +2,19 @@
 const fs = require("fs");
 const express = require("express");
 const path = require("path");
+const port = 3000;
+
+
+const app = express()
+app.use(express.json());
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/', (req, res) => {
+    res.send('Hello')
+  });
+  
+  app.listen(port, () => {
+    console.log(`listening on port ${port}!`)
+  });
