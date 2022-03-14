4a513d3f5d7e82d3c583bbcc1e61ad9e96acf3e9
Sarah Lloyd <sarahlloyd407a@gmail.com>
2022-03-14 16:42:51 +0000

    Get and Post Requests done

--- server.js
+++ server.js
@@ -1,47 +1,55 @@
 // Packages and Dependencies
+
 const fs = require("fs");
 const express = require("express");
 const path = require("path");
 const server = require("http");
+const { v4: uuidv4 } = require('uuid');
 const port = process.env.PORT || 8080;
 
 // Setting Up Server
+
 const app = express()
 app.use(express.json());
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
 // GET Requests
-app.get("public/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/notes"));
-});
 
-app.get("/api/notes", (req, res) => {
-res.sendFile(path.join(__dirname, "/db/db.json"));
-});
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
+})
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
-});
-  
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+})
+
+app.get("/api/notes", function (req, res) {
+  fs.readFile("./db/db.json", function (err, data) {
+      if (err) throw err;
+      res.json(JSON.parse(data));
+  })
+})
+
 // POST Requests
 
-app.get("/api/notes", function(req, res) {
-  readFileAsync("./db/db.json", "utf8").then(function(data) {
-      notes = [].concat(JSON.parse(data))
-      res.json(notes);
-    })
-}); 
-
-app.post("/api/notes", (req, res) => {
-  let Note = req.body;
-  let noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-  let noteSize = (noteList.length).toString();
-  Note.id = noteSize;
-  noteList.push(Note);
-  fs.writeFileSync("./db/db.json", JSON.stringify(noteList));
-  res.json(noteList);
+app.post("/api/notes", function (req, res) {
+  let createNote = req.body;
+  createNote.id = uuidv4();
+  fs.readFile("./db/db.json", function (err, data) {
+      if (err) throw err;
+      let notes = JSON.parse(data);
+      notes.push(createNote);
+      fs.writeFile("./db/db.json", JSON.stringify(notes), function (err) {
+          if (err) throw err;
+          res.status(200).end();
+      })
+  })
 })
+
+//DELETE Requests
+
+
 
 app.listen(port, "0.0.0.0", function() {
   console.log("App listening on PORT " + port);
